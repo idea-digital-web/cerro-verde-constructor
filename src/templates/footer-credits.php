5391d6d36ddc87793ceab1a2476a9eaa445c3391
Alex Ballera <alexballera@gmail.com>
2016-08-17 15:14:57 -0400

    Mejorado estilo de login.php

--- src/templates/footer-credits.php
+++ src/templates/footer-credits.php
@@ -1,5 +1,5 @@
 <div class="site-footer__credits">
 	<!-- <span><?php bloginfo('name'); ?></span> -->
-	<span>Textiles Licratex C.A. J-299969842 © 2016</span>
+	<span>CERRO VERDE DIGITAL, C.A. J-407920561 © 2016</span>
 	<span>Desarrollado por <a href="http://www.ideadigital.com.ve/" target="_blank">Idea Digital</a></span>
 </div>
