3c9967533ea6253c604ed608d8e361a67d3f6c80
Alex Ballera <alexballera@gmail.com>
2016-08-20 18:36:12 -0400

    Arreglando error en custom-field-billing

--- src/inc/custom-field-billing.php
+++ src/inc/custom-field-billing.php
@@ -1,7 +1,9 @@
-<!-- https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/ -->
-<!-- https://surpriseazwebservices.com/edit-woocommerce-checkout-fields/ -->
 <?php 
+/**
+https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/ 
+https://surpriseazwebservices.com/edit-woocommerce-checkout-fields/
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+*/
 
 function custom_override_checkout_fields( $fields ) {
 	unset($fields['billing']['billing_address_2']);
