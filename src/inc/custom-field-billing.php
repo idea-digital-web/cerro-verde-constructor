8a1fa6d45fee8f5cce6f37811bebcc5615f0cf48
Alex Ballera <alexballera@gmail.com>
2016-08-20 08:09:18 -0400

    Quitado pais de la pagina de facturacion

--- src/inc/custom-field-billing.php
+++ src/inc/custom-field-billing.php
@@ -6,10 +6,10 @@ function custom_override_checkout_fields( $fields ) {
 	// unset($fields['billing']['billing_last_name']);
 	// unset($fields['billing']['billing_company']);
 	// unset($fields['billing']['billing_address_1']);
-	unset($fields['billing']['billing_address_2']);
 	// unset($fields['billing']['billing_city']);
+	unset($fields['billing']['billing_address_2']);
 	unset($fields['billing']['billing_postcode']);
-	// unset($fields['billing']['billing_country']);
+	unset($fields['billing']['billing_country']);
 	// unset($fields['billing']['billing_state']);
 	// unset($fields['billing']['billing_phone']);
 	// unset($fields['order']['order_comments']);
