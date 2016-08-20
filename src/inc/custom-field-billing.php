c4a5d1802a7a719c773b32dabe96512c262c360a
Alex Ballera <alexballera@gmail.com>
2016-08-20 16:57:47 -0400

    Cambiada etiquetas ciudad y estado en facturacion

--- src/inc/custom-field-billing.php
+++ src/inc/custom-field-billing.php
@@ -1,3 +1,4 @@
+<!-- https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/ -->
 <?php 
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
@@ -19,6 +20,11 @@ function custom_override_checkout_fields( $fields ) {
 	// unset($fields['billing']['billing_last_name']);
 	// unset($fields['billing']['billing_email']);
 	// unset($fields['billing']['billing_city']);
+
+	$fields['billing']['billing_state']['label'] = 'Estado';
+	$fields['billing']['billing_city']['label'] = 'Ciudad';
+
+	return $fields;
 
 return $fields;
 }
