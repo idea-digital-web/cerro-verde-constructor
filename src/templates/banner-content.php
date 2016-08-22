5f5719c1c3cc6a2195cd8153149fc10b702b4e50
Alex Ballera <alexballera@gmail.com>
2016-08-22 11:52:09 -0400

    Agregando alt en img tag

--- src/templates/banner-content.php
+++ src/templates/banner-content.php
@@ -1,11 +1,11 @@
 <div class="content-area__slides">
   <picture>
-    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-1.png" />
+    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-1.png" alt="Slider Content" />
   </picture>
   <picture>
-    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-2.png" />
+    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-2.png" alt="Slider Content" />
   </picture>
   <picture>
-    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-3.png" />
+    <img src="http://i0.wp.com/cerroverde.ideapruebas.com/wp-content/uploads/2016/08/banner-content-3.png" alt="Slider Content" />
   </picture>
 </div>
