965d6f75df086c5d379a3d2c0f756e531cb54dbc
Alex Ballera <alexballera@gmail.com>
2016-08-23 12:50:53 -0400

    Editado plugins

--- src/js/lib/changueAttr.js
+++ src/js/lib/changueAttr.js
@@ -1,10 +1,4 @@
 jQuery(function (jQuery) {
-    jQuery('.attachment-shop_catalog.wp-post-image.lazy').attr(
-        'alt', 'Producto Cerroverde'
-        )
-    jQuery('noscript>img.attachment-shop_catalog.wp-post-image.lazy').attr(
-        'alt', 'Producto Licratex'
-        )
     jQuery('nav').removeAttr('role')
     jQuery('footer').removeAttr('role')
     jQuery('input').removeAttr('tooltip')
