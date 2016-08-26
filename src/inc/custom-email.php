3767192c60f23d288bd82bd3aacedf3d191d6ef2
Alex Ballera <alexballera@gmail.com>
2016-08-26 16:39:25 -0400

    Cambiado correo

--- src/inc/custom-email.php
+++ src/inc/custom-email.php
@@ -3,7 +3,7 @@ add_filter('wp_mail_from', 'new_mail_from');
 add_filter('wp_mail_from_name', 'new_mail_from_name');
 
 function new_mail_from($old) {
- return 'alexballera@gmail.com';
+ return 'cerroverde.master@gmail.com';
 }
 function new_mail_from_name($old) {
  return 'Cerro Verde Tienda Digital';
