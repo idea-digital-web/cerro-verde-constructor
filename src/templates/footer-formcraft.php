3544a2d13b2662a2c0d0f31d957d9bdc0585a14d
Alex Ballera <alexballera@gmail.com>
2016-09-15 10:39:39 -0400

    Optimizando el codigo

---
+++ src/templates/footer-formcraft.php
@@ -0,0 +1,5 @@
+	<?php if (get_bloginfo( 'url' ) === "http://localhost/cerro-verde"){
+	if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); };
+	} else {
+	if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); };
+	} ?>
